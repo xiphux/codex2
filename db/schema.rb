7483e47620c26d5789baabcb153319e795058a54
Chris Han <xiphux@gmail.com>
2012-05-05 16:34:52 -0500

    Add series model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505213053) do
+ActiveRecord::Schema.define(:version => 20120505213329) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(:version => 20120505213053) do
 
   create_table "genres", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "series", :force => true do |t|
+    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
