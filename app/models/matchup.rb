0c448acab1933cfc75c609b49602ac0d26ca0d36
Chris Han <xiphux@gmail.com>
2012-05-05 16:52:43 -0500

    Add matchup model

---
+++ app/models/matchup.rb
@@ -0,0 +1,3 @@
+class Matchup < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505213745) do
+ActiveRecord::Schema.define(:version => 20120505215101) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(:version => 20120505213745) do
 
   create_table "genres", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "matchups", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
