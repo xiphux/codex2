e227ea6f4951bbfd0ca0a0e93411135487409f57
Chris Han <xiphux@gmail.com>
2012-05-12 15:06:19 -0500

    Increment view counts

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,6 +3,8 @@ class ChaptersController < ApplicationController
   def show
     @fic = Fic.find(params[:fic_id])
     @chapter = Chapter.select(:data).find(:first, :conditions => { :fic_id => params[:fic_id], :number => params[:id] })
+    @chapter.views += 1
+    @chapter.save()
 
     respond_to do |format|
       format.html
