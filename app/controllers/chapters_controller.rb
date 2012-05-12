31228907d650995b9ae3bfc2e846a4802afce722
Chris Han <xiphux@gmail.com>
2012-05-12 12:03:13 -0500

    Use find instead of all to look up chapter

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,7 +2,7 @@ class ChaptersController < ApplicationController
 
   def show
     @fic = Fic.find(params[:fic_id])
-    @chapter = Chapter.select(:data).all(:conditions => { :fic_id => params[:fic_id], :number => params[:id] }).first
+    @chapter = Chapter.select(:data).find(:first, :conditions => { :fic_id => params[:fic_id], :number => params[:id] })
 
     respond_to do |format|
       format.html
