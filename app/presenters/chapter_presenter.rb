3ea126ec61541dae198485320294b70353a3f58b
Chris Han <xiphux@gmail.com>
2012-05-16 21:49:39 -0500

    Use direct modification to decrease the number of large string copies

--- app/presenters/chapter_presenter.rb
+++ app/presenters/chapter_presenter.rb
@@ -7,11 +7,19 @@ class ChapterPresenter
     @chapter = chapter
   end
 
-  def compact_blank_lines(text)
-    text.gsub(/([^\n])(\n\s*){2,}\n(\s*[^\n])/) { |match| $1 + "\n\n" + $3 }
+  def compact_blank_lines!(text)
+    text.gsub!(/([^\n])(\n\s*){2,}\n(\s*[^\n])/) { |match| $1 + "\n\n" + $3 }
   end
 
-  def unwrap(text)
+  def compact_blank_lines(text)
+    compacted_text = text.dup
+    
+    compact_blank_lines!(compacted_text)
+
+    compacted_text
+  end
+
+  def unwrap!(text)
     lines = compact_blank_lines(text).split("\n")
 
     line_count = lines.count
@@ -37,50 +45,42 @@ class ChapterPresenter
 
     space_to_line_ratio = space_ended_line_count.to_f / content_line_count.to_f
 
-    unwrapped_text = text
-
     if space_to_line_ratio > 0.6 && space_to_line_ratio < 0.98 then
       # text has lots of lines that end in spaces
       # might be unwrappable by using the ending space
       # as an indication of text continuing on the next line
-      unwrapped_text = text.gsub(/ \n/, " ")
+      text.gsub!(/ \n/, " ")
     elsif (indented_line_count.to_f / content_line_count.to_f) > 0.20 then
       # text appears to have a number of indented lines
       # try to use indents to find paragraphs
-      unwrapped_text = text.gsub(/ *\n(\S)/) { |match| " " + $1 }
+      text.gsub!(/ *\n(\S)/) { |match| " " + $1 }
     elsif (((line_count - content_line_count).to_f / line_count.to_f) > 0.10) then
       # text appears to have a number of blank lines
       # try to use blank lines to find paragraphs
-      unwrapped_text = text.gsub(/([^\n]) *\n([^\r\s])/) { |match| $1 + " " + $2 }
+      text.gsub!(/([^\n]) *\n([^\r\s])/) { |match| $1 + " " + $2 }
     elsif (average_length > 1) then
       # try to unwrap by guessing the width of lines
       regex = Regexp.new("([^\n]{" + average_length.to_i.to_s + ",})\n")
-      unwrapped_text = text.gsub(regex) { |match| $1 + " " }
-      unwrapped_text = unwrapped_text.gsub(/([\w,]) {2,}([\w,])/) { |match| $1 + " " + $2 }
+      text.gsub!(regex) { |match| $1 + " " }
+      text.gsub!(/([\w,]) {2,}([\w,])/) { |match| $1 + " " + $2 }
     end
 
     # preserve breaklines that may have accidentally been wrapped up
-    unwrapped_text.gsub!(/([^\*\-=\~_<>\^A-Z])((([\*\-=\~_<>\^]) ?){3,})([^\*\-=\~_<>\^A-Z])/) { |match| $1 + "\n" + $2 + "\n" + $5 }
-
-    unwrapped_text
+    text.gsub!(/([^\*\-=\~_<>\^A-Z])((([\*\-=\~_<>\^]) ?){3,})([^\*\-=\~_<>\^A-Z])/) { |match| $1 + "\n" + $2 + "\n" + $5 }
   end
 
-  def stylize(text)
-    stylized = text.dup
-
+  def stylize!(text)
     # stylize breaking lines
-    stylized.gsub!(/\s*\n([\s\n])*(([\*\-=\~_<>\^]) ?){3,}([\s\n])*\n/, "<div class=\"breakline\"></div>")
+    text.gsub!(/\s*\n([\s\n])*(([\*\-=\~_<>\^]) ?){3,}([\s\n])*\n/, "<div class=\"breakline\"></div>")
 
     # stylize underscore emphasis
-    stylized.gsub!(/(\W)_(\S+?)_(\W)/) { |match| $1 + '<span class="emphasis">' + $2 + '</span>' + $3 }
+    text.gsub!(/(\W)_(\S+?)_(\W)/) { |match| $1 + '<span class="emphasis">' + $2 + '</span>' + $3 }
 
     # stylize asterisk emphasis
-    stylized.gsub!(/([^\*])(\*{1,2})([^\*>\n]*)(\*{1,2})([^\*])/) { |match| $1 + $2 + '<span class="emphasis">' + $3 + '</span>' + $4 + $5 }
+    text.gsub!(/([^\*])(\*{1,2})([^\*>\n]*)(\*{1,2})([^\*])/) { |match| $1 + $2 + '<span class="emphasis">' + $3 + '</span>' + $4 + $5 }
 
     # stylize trademark symbol
-    stylized.gsub!(/\^?\(TM\)/i, "<sup>TM</sup>")
-
-    stylized
+    text.gsub!(/\^?\(TM\)/i, "<sup>TM</sup>")
   end
 
   def content
@@ -106,14 +106,14 @@ class ChapterPresenter
 
         # unwrap lines
         if @chapter.wrapped then
-          text = unwrap(text)
+          unwrap!(text)
         end
 
       else
 
         # unwrap lines
         if @chapter.wrapped then
-          text = unwrap(text)
+          unwrap!(text)
         end
 
         # pad dense text with extra space
@@ -124,10 +124,10 @@ class ChapterPresenter
       end
 
       # compact extra whitespace lines
-      text = compact_blank_lines(text)
+      compact_blank_lines!(text)
 
       # convert text styles into html styles
-      text = stylize(text)
+      stylize!(text)
 
       # sanitize html excluding stylized tags
       text = sanitize(text, :tags => %w(div span sup))
